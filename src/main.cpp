213f76a16b4d4525cad9a3745a1db6956552760c
Heikki Salo <heikki.ao.salo@iki.fi>
2018-03-03 13:00:31 +0200

    Instanssi lights & demo specific stuff

--- src/main.cpp
+++ src/main.cpp
@@ -15,17 +15,78 @@
 #define USE_MIPMAPS 1
 #define USE_AUDIO   1
 #define NO_UNIFORMS 0
+#define USE_INSTANSSI_LIGHTS 1
 
 #include "definitions.h"
 #if OPENGL_DEBUG
 	#include "debug.h"
 #endif
 
+
 #include "glext.h"
 #include "shaders/fragment.inl"
 #if TWO_PASS
 	#include "shaders/post.inl"
 #endif
+
+#if USE_INSTANSSI_LIGHTS
+	#include<stdio.h>
+	#include<winsock2.h>
+
+	#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+	#define LIGHT_SERVER "192.168.69.10"  //address of the server
+	//#define LIGHT_SERVER "10.0.0.46"  //address of the server
+    #define LIGHT_PORT 9909
+	#define LIGHT_COUNT 24
+	#define LIGHT_BUFFER_SIZE (1 + (LIGHT_COUNT * 6))
+	//#define LIGHT_PACKET_EVERY_N_SAMPLES 2205;
+	#define LIGHT_PACKET_INTERVAL 0.05
+#endif
+
+// Satisfy linker cries
+
+extern "C"
+{
+	int _fltused = 1;
+}
+
+extern "C"
+{
+	// https://www.codeproject.com/articles/383185/sse-accelerated-case-insensitive-substring-search
+	int __isa_enabled = 0;
+}
+
+extern "C"
+{
+	int __favor = 0;
+}
+
+
+#pragma code_seg(".envelope")
+extern "C" float get_Envelope(int instrument)
+{
+	return (&_4klang_envelope_buffer)[((MMTime.u.sample >> 8) << 5) + 2 * instrument];
+}
+
+#if USE_INSTANSSI_LIGHTS
+// https://instanssi.org/2018/valot/
+void allLightsToRGB(char* packet, int r, int g, int b)
+{
+	packet[0] = 1; // Version of spec = 1
+	for (int iLight = 0; iLight < LIGHT_COUNT; iLight++)
+	{
+		int segmentI = 1 + iLight * 6;
+		packet[segmentI + 0] = 1; // effect type = 0 - light
+		packet[segmentI + 1] = iLight; // light index
+		packet[segmentI + 2] = 0; // type of light = 0 - rgb
+		packet[segmentI + 3] = min(r, 255); // r
+		packet[segmentI + 4] = min(g, 255); // g
+		packet[segmentI + 5] = min(b, 255); // b
+	}
+}
+#endif
+
 
 #ifndef EDITOR_CONTROLS
 void entrypoint(void)
@@ -34,6 +95,45 @@ void entrypoint(void)
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 #endif
 {
+	#if USE_INSTANSSI_LIGHTS
+		// https://www.binarytides.com/udp-socket-programming-in-winsock/
+		struct sockaddr_in si_other;
+		int s, slen = sizeof(si_other);
+		char packet[LIGHT_BUFFER_SIZE];
+		WSADATA wsa;
+		char udpInfo[2048];
+		float lastLightPacketAt = 0.0; // s
+
+		//Initialise winsock
+		if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		{
+			#if DEBUG
+			//sprintf(udpInfo, "Failed. Error Code : %d", WSAGetLastError());
+			MessageBox(NULL, udpInfo, "", 0x00000000L);
+			ExitProcess(1);
+			#endif
+		}
+
+		//create socket
+		if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
+		{
+			#if DEBUG
+			//sprintf(udpInfo, "socket() failed with error code : %d", WSAGetLastError());
+			MessageBox(NULL, "socket creation failed", "", 0x00000000L);
+			ExitProcess(1);
+			#endif
+		}
+
+		memset((char *)&si_other, 0, sizeof(si_other));
+		si_other.sin_family = AF_INET;
+		si_other.sin_port = htons(LIGHT_PORT);
+		si_other.sin_addr.S_un.S_addr = inet_addr(LIGHT_SERVER);
+
+		//if (sendto(s, packet, LIGHT_BUFFER_SIZE, 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
+		allLightsToRGB(packet, 60, 0, 0);
+		sendto(s, packet, LIGHT_BUFFER_SIZE, 0, (struct sockaddr *) &si_other, slen);
+	#endif
+
 	// initialize window
 	#if FULLSCREEN
 		ChangeDisplaySettings(&screenSettings, CDS_FULLSCREEN);
@@ -96,11 +196,40 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 			#if USE_AUDIO
 				waveOutGetPosition(hWaveOut, &MMTime, sizeof(MMTIME));
 				// it is possible to upload your vars as vertex color attribute (gl_Color) to save one function import
+
+				float timeNow = float(MMTime.u.sample) / SAMPLE_RATE;
+				float kick = get_Envelope(0); // kick
+				float snare = get_Envelope(1); // snare
 				#if NO_UNIFORMS
 					glColor3ui(MMTime.u.sample, 0, 0);
 				#else
 					// remember to divide your shader time variable with the SAMPLE_RATE (44100 with 4klang)
 					((PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i"))(0, MMTime.u.sample);
+					float songInfo[2];
+					songInfo[0] = kick;
+					songInfo[1] = snare;
+					float misc[2];
+					misc[0] = float(MMTime.u.sample) / 22000.0f;
+					misc[1] = float(MMTime.u.sample) / 11000.0f;
+					((PFNGLUNIFORM1FVPROC)wglGetProcAddress("glUniform2fv"))(1, 1, songInfo); //
+					((PFNGLUNIFORM1FVPROC)wglGetProcAddress("glUniform2fv"))(2, 1, misc); //
+
+
+				#endif
+				#if USE_INSTANSSI_LIGHTS
+					float timeSinceLastPacket = timeNow - lastLightPacketAt;
+					if (timeSinceLastPacket >= LIGHT_PACKET_INTERVAL)
+					{
+						if (timeNow >= 19.0 && snare > 0.2) {
+							int l = int(snare * 255.0);
+							allLightsToRGB(packet, l, l, l);
+						}
+						else {
+							allLightsToRGB(packet, 60, 0, 0);
+						}
+						sendto(s, packet, LIGHT_BUFFER_SIZE, 0, (struct sockaddr *) &si_other, slen);
+						lastLightPacketAt = timeNow;
+					}
 				#endif
 			#endif
 		#else
@@ -147,11 +276,16 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 				}
 			}
 		#endif
-	} while(!GetAsyncKeyState(VK_ESCAPE)
+	} while(!GetAsyncKeyState(VK_ESCAPE) && (MMTime.u.sample / SAMPLE_RATE) <= 42.0
 		#if USE_AUDIO
 			&& MMTime.u.sample < MAX_SAMPLES
 		#endif
 	);
+
+	#if USE_INSTANSSI_LIGHTS
+		closesocket(s);
+		WSACleanup();
+	#endif
 
 	ExitProcess(0);
 }
